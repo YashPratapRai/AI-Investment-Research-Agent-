faf25fbee2b3b7462c6c527ef5782e19a9b40df5
raiyashpratap@gmail.com <raiyashpratap@gmail.com>
2026-06-29 01:48:00 +0530

    updated readme

--- server/src/config/groq.js
+++ server/src/config/groq.js
@@ -2,7 +2,7 @@ import { ChatGroq } from "@langchain/groq";
 
 const groq = new ChatGroq({
   apiKey: process.env.GROQ_API_KEY,
-  model: "llama-3.3-70b-versatile",
+  model: "llama-3.1-8b-instant",
   temperature: 0.2,
 });
 
